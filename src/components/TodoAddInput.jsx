9fe54ca270a09e5368660db1ca14ad1d978aed9d
oxlzlo <yeojin58@naver.com>
2024-11-11 17:56:42 +0900

    design: 투두 추가 인풋 디자인 구현

---
+++ src/components/TodoAddInput.jsx
@@ -0,0 +1,11 @@
+const TodoAddInput = () => {
+  return (
+    <input
+      type="text"
+      className="todoAddInput"
+      placeholder="할 일을 추가하세요"
+    />
+  );
+};
+
+export default TodoAddInput;
