adfbc6923ee36ec0fafc292a0c7dc85b2300486b
oxlzlo <yeojin58@naver.com>
2024-11-12 04:07:05 +0900

    fix: 처음에 같은 데이터가 두번 조회되는 오류 수정

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,22 +3,13 @@ import useFetchTodos from "../../hooks/useFetchTodos";
 import useTodoDelete from "../../hooks/useTodoDelete";
 import TodoItem from "./TodoItem";
 
-const TodoList = ({ todos }) => {
+const TodoList = () => {
   const { data } = useFetchTodos();
   const { deleteTodo } = useTodoDelete();
 
   return (
     <div className="todoItem">
       <div className="todoItem_list">
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            id={todo.id}
-            title={todo.title}
-            completed={todo.completed}
-            onDelete={() => deleteTodo(todo.id)}
-          />
-        ))}
         {data?.pages.map((page, pageIndex) => (
           <div key={pageIndex}>
             {page.map((todo) => (
